f34e5702a0643a48d6c946b27a9f9900309211dd
Nishant Racherla <nishant.racherla@gmail.com>
2022-11-26 21:43:14 -0800

    finished phase 3

--- w8d5/canvas_practice/practice.js
+++ w8d5/canvas_practice/practice.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const canvas = document.getElementById("mycanvas");
+  const canvas = document.getElementById('mycanvas');
   canvas.height = 500;
   canvas.width = 500;
   const ctx = canvas.getContext('2d');
   ctx.fillStyle('red');
   ctx.fillRect(0, 0, 500, 500);
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+  ctx.strokeStyle = 'yellow';
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  ctx.fillStyle = 'blue';
+  ctx.fill();
 });
